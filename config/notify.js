dab515586495777a313700f74c86fcc187cfd9f4
David Tabacco <David_Tabacco@merck.com>
2015-10-28 21:25:17 -0400

    reset Password functionality

--- config/notify.js
+++ config/notify.js
@@ -6,7 +6,7 @@ exports.default = {
         password: process.env.BRIM_EMAIL_PASSWORD,
 
     	senderAddress: '"Password Reset" <partialpallet@gmail.com>',
-    	baselink: "www.partialpallet.com/resetPassword.html?sessionid=",
+    	baselink: "www.partialpallet.com/resetpassword.html?sessionid=",
     }
   }
 }
